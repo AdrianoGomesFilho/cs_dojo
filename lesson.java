d40675e40902381c1d40e499bd283c22711ab1a7
Adriano G <adrianogomesfilho98@gmail.com>
2023-12-23 15:40:26 -0300

    first commit

---
+++ lesson.java
@@ -0,0 +1,21 @@
+class Robot {
+    String name;
+    String color;
+    int weight;
+
+Robot(String n, String color, int w) {
+    this.name = n;
+    this.color = c;
+    this.weight = w;
+}
+
+void introduceSelf() {
+    System.out.printIn(
+        "My name is" + this.name);
+}
+
+}
+
+
+Robot r1 = new Robot("Cabeca de gelo", "yellow", 900);
+Robot r2 = new Robot("Ice Head", "blue", 1); 
